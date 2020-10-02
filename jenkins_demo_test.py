d6daedc805ba9abc3980763466250c2168e1ce2d
jine <306262361@qq.com>
2020-10-02 10:24:06 +0800

    mac更改

--- jenkins_demo_test.py
+++ jenkins_demo_test.py
@@ -15,68 +15,38 @@ from selenium.webdriver.chrome.options import Options
 class TestDemo():
 
     def setup(self):
-
-        #namepath = os.path.abspath(os.path.dirname(__file__))
-
-        #projectpath = namepath[:namepath.find("jenkis_ui_auto\\")] + "jenkis_ui_auto\\"
-
         path='/usr/local/chromedriver'
-        
         try:
-
             using_headless=os.environ['using_headless']
-
         except KeyError:
-
             using_headless=None
-
             print('no using_headless')
-
         options=Options()
-
         if using_headless is not None and using_headless.lower()=='true':
-
             options.add_argument('--headless')
-
         options.add_argument('--no-sandbox')
-
         self._driver = webdriver.Chrome(executable_path=path,options=options)
-
-       
-
         self._driver.get('https://www.baidu.com/s?wd=%E7%99%BE%E5%BA%A6&ie=utf-8&tn=78040160_5_pg&ch=12')
 
-
-
     def teardown(self):
-
         self._driver.quit()
 
-        
-    @pytest.mark.parametrize('value', ['百度', '顺丰', '京东', '淘宝'])
+    @pytest.mark.parametrize('value', ['百度', '顺丰'])
     def test_demo(self, value):
-
-        text = self._driver.find_element_by_xpath('//*[@id="1"]/h3/a[1]').text
-
-        self._driver.find_element_by_xpath('//*[@id="kw"]').clear()
-
-        self._driver.find_element_by_xpath('//*[@id="kw"]').send_keys(value)
-
-        self._driver.find_element_by_xpath('//*[@id="su"]').click()
-
-        sleep(3)
-
-
+        #搜索pytest传参
+        self.search(value)
 
     def test_demo1(self, value='平安'):
+        #搜索简单传参
+        self.search(value)
 
+    def search(self,value):
+        #搜索方法
         text = self._driver.find_element_by_xpath('//*[@id="1"]/h3/a[1]').text
-
         self._driver.find_element_by_xpath('//*[@id="kw"]').clear()
-
         self._driver.find_element_by_xpath('//*[@id="kw"]').send_keys(value)
-
         self._driver.find_element_by_xpath('//*[@id="su"]').click()
-
         sleep(3)
 
+if __name__ == '__main__':
+    pass
